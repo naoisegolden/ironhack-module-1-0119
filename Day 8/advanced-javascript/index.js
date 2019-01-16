a717655bb1d233ecd8ba0a783390e0f257a0f414
Naoise Golden <naoise.golden@gmail.com>
2019-01-16 10:47:36 +0100

    Add advanced js lecture

---
+++ Day 8/advanced-javascript/index.js
@@ -0,0 +1,50 @@
+var obj1 = {
+    name: "Naoise",
+    greet: function() {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+};
+
+function outsideGreet(isExcited, myName) {
+    console.log(`Hello ${myName}, my name is ${this.name}${isExcited ? "!!" : ""}`);
+}
+
+var obj2 = {
+    name: "Breogan"
+};
+
+obj1.greet();
+outsideGreet(true);
+outsideGreet.call(obj2, true, "Donald");
+outsideGreet.apply(obj2, [true, "Donald"]); // 'a' for apply and for array
+
+function someFunction() {
+    console.log(arguments);
+    outsideGreet.apply(obj2, arguments)
+}
+someFunction();
+someFunction(true);
+someFunction(true, "Donald");
+
+var obj2Greet = outsideGreet.bind(obj2);
+obj2Greet(true, "Donald");
+
+// ---------------
+
+// Super common to have to bind(this) to async callbacks/handlers
+
+class Chronometer {
+    constructor() {
+        this.seconds = 0;
+    }
+    startTimer() {
+        var self = this;
+        setTimeout(function() {
+            debugger; // what is `this`?
+            this.seconds++;
+        }.bind(this, 1000);
+    }
+}
+
+var chronometer = new Chronometer();
+chronometer.startTimer();
